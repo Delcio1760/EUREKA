b293b6810fb7de2d5e08d0b71c2851cbb1e05bc8
delcio cunha <delcioaugustocunha@gmail.com>
2025-05-19 21:12:13 +0100

    Atualização de todos os modals e correção de erros

--- JS/models/adminModel.js
+++ JS/models/adminModel.js
@@ -1,42 +1,45 @@
 const LS_KEY = "admin";
 let admins = [];
 
+
 export function init(){
-    admins = JSON.parse(localStorage.getItem(LS_KEY)) || [];
+   admins = JSON.parse(localStorage.getItem(LS_KEY)) || [];
 }
+
 
 // Adicionar Administrador
 export function addAdmin(username, email, password) {
-    if(admins.some((admin) => admin.username === username)){
-        throw Error(`User with username ${username} already exists`);
-    }else{
-        admins.push(new Admin(username, email, password));
-        localStorage.setItem(LS_KEY, JSON.stringify(admins));
-    }
+   if(admins.some((admin) => admin.username === username)){
+       throw Error(`User with username ${username} already exists`);
+   }else{
+       admins.push(new Admin(username, email, password));
+       localStorage.setItem(LS_KEY, JSON.stringify(admins));
+   }
 }
 
 export function loginAdmin(username, password){
-    const admin = admins.find(a => a.username === username && a.password === password);
-    if(!admin) throw Error("Credenciais invalidas");
+   const admin = admins.find(a => a.username === username && a.password === password);
+   if(!admin) throw Error("Credenciais invalidas");
 
-    sessionStorage.setItem("admin", JSON.stringify(admin));
-    
-    // Redericiona para o Dasboard
-    window.location.href = "/dashboard.html";
+   sessionStorage.setItem("admin", JSON.stringify(admin));
+  
+   // Redericiona para o Dasboard
+   window.location.href = "/dashboard.html";
 }
 
-//Logout do Utlizador
-export function logout() {
-    sessionStorage.removeItem("loggedInUser");
+//Logout do Administrador
+export function logoutAdmin() {
+   sessionStorage.removeItem("admin");
 }
+
 class Admin{
-    username = "";
-    email = "";
-    password = "";
+   username = "";
+   email = "";
+   password = "";
 
-    constructor(username, email, password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+   constructor(username, email, password) {
+       this.username = username;
+       this.email = email;
+       this.password = password;
+   }
 }
--- JS/models/teacherModel.js
+++ JS/models/teacherModel.js
@@ -1,73 +1,74 @@
-const LS_KEY = "explicadores";
+const LS_KEY = "explicador";
 
-let users ;
- // Caregar utlizadores da LocalStorage
- export function init(){
-    users = JSON.parse(localStorage.getItem(LS_KEY)) || [];
- }
+let explicadores = [];
+// Caregar explicadores da LocalStorage
+export function init(){
+   explicadores = JSON.parse(localStorage.getItem(LS_KEY)) || [];
+}
 
- // Adicionar explicador
+// Adicionar explicador
 export function addExplicador(username,email,password,idade,disciplinas,morada,modalidade,preco) {
-    if(users.some((user) => user.username === username)){
-        throw Error(`User with username ${username} already exists`);
-    }else{
-    users.push(new Explicador(username,email,password,idade,disciplinas,morada,modalidade,preco));
-    localStorage.setItem(LS_KEY, JSON.stringify(users));}
+  
+   if(explicadores.some((user) => user.username === username)){
+       throw Error(`User with username ${username} already exists`);
+  
+   }else{
+   explicadores.push(new Explicador(username,email,password,idade,disciplinas,morada,modalidade,preco));
+   localStorage.setItem(LS_KEY, JSON.stringify(explicadores));}
 
 }
 
 
 // Login do Explicador
-export function login(username, password) {
-    const user = users.find(
-        (user) => user.username === username && user.password === password
-    );
-    if (user) {
-        sessionStorage.setItem("loggedInUser", JSON.stringify(user));
-        return true;
-    } else {
-        throw Error("Invalid username or password");
-    }            
+export function loginExplicador(username, password) {
+   const user = explicadores.find(
+       (user) => user.username === username && user.password === password);
+
+       if (user) {
+       sessionStorage.setItem("loggedTutor", JSON.stringify(user));
+       return true;
+       } else {
+       throw Error("Invalid username or password");
+        }           
 }
 
 
 // Logout do Explicador
-export function logout() {
-    sessionStorage.removeItem("loggedInUser");
+export function logoutexplicador() {
+   sessionStorage.removeItem("loggedTutor");
 }
 
-// VERIFICA EXISTÊNCIA DE ALGUÉM AUTENTICADO
-export function isLoggedIn() {
-    return sessionStorage.getItem("loggedInUser") ? true : false;
+// VERIFICA EXISTÃŠNCIA DE ALGUÃ‰M AUTENTICADO
+export function isTutorLoggedin() {
+   return sessionStorage.getItem("loggedTutor") ? true : false;
 }
 
 
 // Devolve o explicador logado
-export function getUserLogged(){
-    return JSON.parse(sessionStorage.getItem("loggedInUser"));
+export function getTutorLogged(){
+   return JSON.parse(sessionStorage.getItem("loggedTutor"));
 }
 
 class Explicador{
-    username = "" ;
-    email = "";
-    password = "";
-    idade = "";
-    disciplinas = "";
-    morada = "";
-    modalidade = "";
-    preco = "";
+   username = "" ;
+   email = "";
+   password = "";
+   idade = "";
+   disciplinas = "";
+   morada = "";
+   modalidade = "";
+   preco = "";
 
-    constructor(username,email,password,idade,disciplinas,morada,modalidade,preco){
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.idade = idade;
-        this.disciplinas = disciplinas;
-        this.morada = morada;
-        this.modalidade = modalidade;
-        this.preco = preco;
-    }
+   constructor(username,email,password,idade,disciplinas,morada,modalidade,preco){
+       this.username = username;
+       this.email = email;
+       this.password = password;
+       this.idade = idade;
+       this.disciplinas = disciplinas;
+       this.morada = morada;
+       this.modalidade = modalidade;
+       this.preco = preco;
+   }
 
-    
+  
 }
-
